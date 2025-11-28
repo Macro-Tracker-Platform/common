d177377a0faef4b5bb31f39fcf5d300082b6e485
Oleg Pruh <olegpruh1@gmail.com>
2025-11-28 15:53:11 +0000

    Add favicon endpoint

---
+++ src/main/java/com/olehprukhnytskyi/controller/FaviconController.java
@@ -0,0 +1,13 @@
+package com.olehprukhnytskyi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FaviconController {
+	@GetMapping("favicon.ico")
+	public ResponseEntity<Void> returnNoFavicon() {
+		return ResponseEntity.ok(null);
+	}
+}
