433e927fbf63ea26cccefd980b13ca0971cb8eb8
Oleg Pruh <olegpruh1@gmail.com>
2025-12-01 23:25:10 +0000

    Remove swagger server

--- src/main/java/com/olehprukhnytskyi/config/SwaggerConfig.java
+++ src/main/java/com/olehprukhnytskyi/config/SwaggerConfig.java
@@ -1,25 +1,20 @@
 package com.olehprukhnytskyi.config;
 
-import com.olehprukhnytskyi.properties.SwaggerProperties;
 import com.olehprukhnytskyi.util.CustomHeaders;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.customizers.OpenApiCustomizer;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableConfigurationProperties(SwaggerProperties.class)
 public class SwaggerConfig {
     private static final Set<String> INTERNAL_HEADERS = Arrays
             .stream(CustomHeaders.class.getDeclaredFields())
@@ -32,7 +27,6 @@ public class SwaggerConfig {
                 }
             })
             .collect(Collectors.toSet());
-    private final SwaggerProperties swaggerProperties;
 
     @Bean
     public OpenApiCustomizer hideInternalHeaders() {
@@ -50,11 +44,11 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
-                .servers(List.of(new Server().url(swaggerProperties.getGatewayBaseUrl())))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
-                                .bearerFormat("JWT")));
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
