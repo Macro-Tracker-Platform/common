1de82fa3f43dccd7aeff6bc55218765a87aa4de7
Oleg Pruh <olegpruh1@gmail.com>
2025-12-16 18:28:18 +0000

    Add EMAIL_NOT_CONFIRMED error code

--- src/main/java/com/olehprukhnytskyi/exception/error/AuthErrorCode.java
+++ src/main/java/com/olehprukhnytskyi/exception/error/AuthErrorCode.java
@@ -10,7 +10,8 @@ public enum AuthErrorCode implements BaseErrorCode {
 	UNSUPPORTED_PROVIDER("Unsupported social provider", HttpStatus.BAD_REQUEST.value()),
 	PROVIDER_API_ERROR("Error from social provider API", HttpStatus.BAD_GATEWAY.value()),
 	TOKEN_VERIFICATION_FAILED("Token verification failed", HttpStatus.UNAUTHORIZED.value()),
-	INVALID_TOKEN("Invalid token", HttpStatus.UNAUTHORIZED.value());
+	INVALID_TOKEN("Invalid token", HttpStatus.UNAUTHORIZED.value()),
+	EMAIL_NOT_CONFIRMED("Email is not confirmed", HttpStatus.FORBIDDEN.value());
 
 	private final String title;
 	private final int status;
