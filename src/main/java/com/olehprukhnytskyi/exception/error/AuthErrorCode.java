1fe4d7fc59de3fb20eefff8a7b4eb371a834816f
Oleg Pruh <olegpruh1@gmail.com>
2025-12-22 16:01:46 +0000

    Add REGISTRATION_DATA_REQUIRED error code

--- src/main/java/com/olehprukhnytskyi/exception/error/AuthErrorCode.java
+++ src/main/java/com/olehprukhnytskyi/exception/error/AuthErrorCode.java
@@ -11,7 +11,9 @@ public enum AuthErrorCode implements BaseErrorCode {
 	PROVIDER_API_ERROR("Error from social provider API", HttpStatus.BAD_GATEWAY.value()),
 	TOKEN_VERIFICATION_FAILED("Token verification failed", HttpStatus.UNAUTHORIZED.value()),
 	INVALID_TOKEN("Invalid token", HttpStatus.UNAUTHORIZED.value()),
-	EMAIL_NOT_CONFIRMED("Email is not confirmed", HttpStatus.FORBIDDEN.value());
+	EMAIL_NOT_CONFIRMED("Email is not confirmed", HttpStatus.FORBIDDEN.value()),
+	REGISTRATION_DATA_REQUIRED("Additional registration data is required for new users",
+			HttpStatus.BAD_REQUEST.value());
 
 	private final String title;
 	private final int status;
