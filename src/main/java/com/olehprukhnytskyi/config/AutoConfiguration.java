928fbd5652f56726b10ba641bd838f8447798f9f
Oleg Pruh <olegpruh1@gmail.com>
2025-12-25 02:01:02 +0000

    Add ShedLockConfig

--- src/main/java/com/olehprukhnytskyi/config/AutoConfiguration.java
+++ src/main/java/com/olehprukhnytskyi/config/AutoConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 		SwaggerConfig.class,
 		IdempotencyAspect.class,
 		FaviconController.class,
+		CommonShedLockConfig.class,
 		GlobalExceptionHandler.class
 })
 public class AutoConfiguration {
---
+++ src/main/java/com/olehprukhnytskyi/config/ShedLockConfig.java
@@ -0,0 +1,30 @@
+package com.olehprukhnytskyi.config;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnBean(DataSource.class)
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
+public class CommonShedLockConfig {
+	@Bean
+	@ConditionalOnMissingBean
+	public LockProvider lockProvider(DataSource dataSource) {
+		return new JdbcTemplateLockProvider(
+				JdbcTemplateLockProvider.Configuration.builder()
+						.withJdbcTemplate(new JdbcTemplate(dataSource))
+						.usingDbTime()
+						.build()
+		);
+	}
+}
