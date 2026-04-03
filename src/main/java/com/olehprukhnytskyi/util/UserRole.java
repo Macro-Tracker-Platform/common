4da33e21a437d71a226d64ee01061f45329c067c
Oleh Prukhnytskyi <oleh.prukhnytskyi@gmail.com>
2026-04-03 18:32:07 +0100

    Add UserRole enum

---
+++ src/main/java/com/olehprukhnytskyi/util/UserRole.java
@@ -0,0 +1,18 @@
+package com.olehprukhnytskyi.util;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "User role")
+public enum UserRole {
+    @Schema(description = "User")
+    USER,
+
+    @Schema(description = "Admin")
+    ADMIN;
+
+    @JsonCreator
+    public static UserRole fromString(String value) {
+        return UserRole.valueOf(value.toUpperCase());
+    }
+}
